41c75007be88607e88c4dec81a0fbf7b6095e587
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-02-15 22:02:12 +0530

    main

--- vikas.js
+++ vikas.js
@@ -1,1 +1,9 @@
 console.log("Vikas Yadav");
+const balance = 1000;
+if (balance<500){
+    console.log("Balance is less then 500");
+}else if(balance<750){
+    console.log("balance is less then 750");
+}else{
+    console.log("balance is less then 2000");
+}
