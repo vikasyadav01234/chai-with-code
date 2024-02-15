2677be700f7c13c9ad1bf5440df688db54aff7ef
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-02-15 22:38:41 +0530

    main

---
+++ truety.js
@@ -0,0 +1,9 @@
+const userEmail = "yadav";
+if (userEmail){
+    console.log("Got user email");
+}else {
+    console.log("Don't have user email");
+}
+let val1;
+val1 = undefined ?? 3;
+console.log(val1)
