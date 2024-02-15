5a195534d0c7018473f7ae806e63e211c792f43d
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-02-15 23:15:26 +0530

    main

--- loops/one.js
+++ loops/one.js
@@ -6,3 +6,15 @@ for (let j = 0; j < 10; j++) {
     console.log(element);
     
 }
+let i=0;
+while(i<10){
+    console.log(`Value of  i is ${i}`);
+    i++;
+}
+do{
+    console.log(`score is ${i}`);
+    i++;
+    if (i==1000){
+        console.log("You are the winner");
+    }
+}while(i<=1000);
