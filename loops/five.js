ec1ed67bd7ef6c9ddce9227b95a45dbd3790b716
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-02-16 12:20:43 +0530

    main

--- loops/five.js
+++ loops/five.js
@@ -4,3 +4,32 @@ const myTotal = myNums.reduce(function(acc, currval){
     return acc+currval
 },0)
 console.log(myTotal);
+
+const mycart =[
+    {
+        name:"js course",
+        price: 1000
+    },
+    {
+        name:"py course",
+        price:1999
+    },
+    {
+        name:"rb course",
+        price:1021
+    },
+    {
+        name:"is course",
+        price:1022
+    },
+    {
+        name:"english course",
+        price:1004
+    },
+    {
+        name:"hindi course",
+        price:10003
+    },
+]
+const pricetopay=mycart.reduce((acc, item) =>acc + item.price,0)
+console.log(pricetopay);
