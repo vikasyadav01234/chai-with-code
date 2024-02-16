2346a757fa666bbaa2327de77f8428aedf9756a9
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-02-16 12:01:33 +0530

    main

--- loops/four.js
+++ loops/four.js
@@ -1,5 +1,5 @@
 const myNums = [1,2,3,4,5,6,7,8,9,10]
 //const newnums = myNums.filter( (num) => num >4)
 //console.log(newnums);
-const newnums = myNums.map((num)=> num+10).map((num)=>num*20)
+const newnums = myNums.map((num)=> num+1).map((num)=>num*10).filter((num)=>num>=40)
 console.log(newnums)
