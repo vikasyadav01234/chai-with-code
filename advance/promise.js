facf5b5e5f4fac314fff60ae03b575f476b5c8d2
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-02-20 11:15:55 +0530

    main

--- advance/promise.js
+++ advance/promise.js
@@ -1,7 +1,9 @@
 const promiseone = new Promise(function(resolve, reject){
     setTimeout(function(){
         console.log("Vikas Yadav")
+        resolve()
     },1000)
+
 })
 
 promiseone.then(function(){
