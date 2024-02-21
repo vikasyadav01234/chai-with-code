1611cce040658e40adb7db6ea5c7368416a57f4f
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-02-21 15:18:38 +0530

    main

---
+++ oops/prototype.js
@@ -0,0 +1,14 @@
+let myheros=["thor","spiderman"]
+
+let heropower = {
+    thor: "hammer",
+    spiderman:"sling",
+    getSpiderPower: function(){
+        console.log(`Spidy Power is ${this.spiderman}`);
+    }
+}
+
+Object.prototype.Vikas = function(){
+    console.log(`Vikas is present in all objects`);
+}
+heropower.Vikas()
