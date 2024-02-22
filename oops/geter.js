e7027b6a00591ed117ddaf25ed5907258dbed82c
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-02-22 19:07:41 +0530

    main

--- oops/geter.js
+++ oops/geter.js
@@ -1,5 +1,16 @@
 class User{
     constructor(email, password){
-        
+        this.email = email;
+        this.password = password
+    }
+    get password(){
+        return this.password.toUpperCase()
+    }
+
+    set password(value){
+        this.password = value
     }
 }
+
+const hitesh = new User ("hitesh.ai","12345")
+console.log(hitesh.password);
