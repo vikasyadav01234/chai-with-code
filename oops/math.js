8a4b27e0e6d93138ce43aa8d4dfff843c69b78a6
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-02-22 12:08:18 +0530

    main

--- oops/math.js
+++ oops/math.js
@@ -13,7 +13,7 @@ console.log(Object.getOwnPropertyDescriptor(chai, "name"));
 Object.defineProperty(chai,'name',{
     //writable:false,
 
-    //enumerable:false
+    enumerable:true
 })
 console.log(Object.getOwnPropertyDescriptor(chai , "name"))
 
